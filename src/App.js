d702cdb51e78365dd77fda14f408055905890aab
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-24 00:51:15 -0700

    Increase Decrease

--- src/App.js
+++ src/App.js
@@ -21,10 +21,15 @@ function  Counter() {
     const newCount = count +1;
     setCount(newCount);
   }
+  const handleDecrease = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  }
   return(
     <div>
           <h1>count:{count}</h1>
           <button onClick = {handleIncrease}>Increase</button>
+          <button onClick = {handleDecrease}>Decrease</button>
     </div>
   )
 }
