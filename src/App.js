7b7b884d0f6a8f5605cd0a2f36cd8e3446916b06
MD Amdadul Islam <58988171+amdadul3036@users.noreply.github.com>
2020-08-24 00:28:44 -0700

    Increase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Hello from './Component/Hello';
@@ -10,10 +10,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+      <Counter></Counter>
       </header>
     </div>
   );
 }
-
+function  Counter() {
+  const[count,setCount] = useState(0);
+  const handleIncrease = () => {
+    const newCount = count +1;
+    setCount(newCount);
+  }
+  return(
+    <div>
+          <h1>count:{count}</h1>
+          <button onClick = {handleIncrease}>Click Here To Increase One</button>
+    </div>
+  )
+}
   export default App;
